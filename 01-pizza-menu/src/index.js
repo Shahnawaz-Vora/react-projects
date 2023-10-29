9f7b7e162f9c3471a7cbc14c6942a3e504ce8e09
Shahnawaz Vora <69008205+Shahnawaz-Vora@users.noreply.github.com>
2023-10-29 18:22:14 +0530

    pizza project

---
+++ 01-pizza-menu/src/index.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+function App() {
+  return (
+    <React.StrictMode>
+      <div className="container">
+        <Header />
+        <Menu />
+        <Footer />
+      </div>
+    </React.StrictMode>
+  );
+}
+function Menu() {
+  const pizzas = pizzaData;
+  return (
+    <div className="menu">
+      <h2>Our Menu</h2>
+      {pizzas.length > 0 ? (
+        <>
+          <p>
+            Authentic italian cuisine. 6 creative dishes to choose from. All
+            from our stove oven, all organic, all delicious
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObject={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on menu Please come back later.</p>
+      )}
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to help you between {openHour}:00 and {closeHour}:00
+        </p>
+      )}
+    </footer>
+  );
+}
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+function Pizza({ pizzaObject }) {
+  if (pizzaObject.soldOut) return null;
+  return (
+    <li className="pizza">
+      <img src={pizzaObject.photoName} alt={pizzaObject.name} />
+      <div>
+        <h3>{pizzaObject.name}</h3>
+        <p>{pizzaObject.ingredients}</p>
+        <p>{pizzaObject.price}</p>
+      </div>
+    </li>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ notes.md
@@ -0,0 +1,17 @@
+- React.StrictMode is only used for developers to render twice to find bugs
+- JSX are used to combine html,css,js in component which is compiled by babel which converted into js
+  -Props are immuatable so it cant changed by child component , if we need that type then we have to use state not props
+  -React flow is top to down directional not bidirectional like angular
+
+-React composition are used to make highly reusable components to reduce prop drilling
+
+**Note** : Watch screenshot for more details
+
+useMemo : memoized value
+useCallback : memoized function
+useState already provide memoized function so no need to memoized that
+
+14 chapter : class based component(remaining optional)
+19 chapter : useMemo , useCallback , context optimization (15 to 18 videos remaining want to do last)
+
+- Redux is same as useReducer and now a days very less use so use whenever needed
